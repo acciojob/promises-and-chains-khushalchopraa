f67fa5143a8eded18e32b678454fc4bf291d36d3
khushalchopraa <advkhushal97@gmail.com>
2024-08-31 18:39:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-//your JS code here. If required.
+
+document.getElementById("ageForm").addEventListener("submit", function(event) {
+  event.preventDefault(); 
+  const name = document.getElementById("name").value;
+  const age = document.getElementById("age").value;
+
+  if (name && age) {
+    new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (age >= 18) {
+          resolve();
+        } else {
+          reject();
+        }
+      }, 4000); 
+    })
+    .then(() => {
+      alert(Welcome, ${name}. You can vote.);
+    })
+    .catch(() => {
+      alert(Oh sorry ${name}. You aren't old enough.);
+    });
+  } else {
+    alert("Please fill in all fields.");
+  }
+});
